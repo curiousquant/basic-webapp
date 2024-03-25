248aac6f9002bca43e7ca2dd529e910499a85495
LAPTOP-FLPBV8FK\jonmu <jguy2591@gmail.com>
2024-03-25 10:56:39 -0400

    update

--- connect.py
+++ connect.py
@@ -1,16 +1,9 @@
 import psycopg2
 
 def connect():
-    pg_connection_dict = {
-        'dbname': 'postgresdb_czir',
-        'user': 'admin',
-        'password': 'PmK1wFxvr4zd003IctgbUACt9bC4U6RE',
-        'port': 5432,
-        'host': 'dpg-cn5ovqmct0pc738hm5kg-a.ohio-postgres.render.com'
-    }
-    
     try:
-        con = psycopg2.connect(**pg_connection_dict)
+        con = psycopg2.connect(dbname='postgresdb_czir',user='admin',password='PmK1wFxvr4zd003IctgbUACt9bC4U6RE',port=5432,
+                               host='dpg-cn5ovqmct0pc738hm5kg-a.ohio-postgres.render.com')
         print("Connected successfully!")
         return con
     except Exception as e:
@@ -33,4 +26,4 @@ def selectItems(connection):
 if __name__ == '__main__':
     #db_config = load_config()
     con = connect()
-    selectItems(con)
+    print(selectItems(con))
